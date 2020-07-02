0c3025aecce9da35bc5fd468be67cb732c122af8
Larissa Fabião <larissafabiao@usp.br>
2020-07-02 15:43:27 -0300

    Erro no JUnit

--- src/br/com/zup/nossacasacodigo/database/DatabaseTest.java
+++ src/br/com/zup/nossacasacodigo/database/DatabaseTest.java
@@ -7,136 +7,97 @@ import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 
 import br.com.zup.nossacasacodigo.author.Author;
 import br.com.zup.nossacasacodigo.book.Book;
 import br.com.zup.nossacasacodigo.category.Category;
 
 public class DatabaseTest {
-	@Test
-	public void testWithOneAutor() {
+	private Database database;
+	private Author author;
+	private Category category;
+	private Book book;
+	
+	@BeforeEach
+	void setup() {
+		this.database = new Database();
+
 		String name = "Larissa Fabião da Fonseca";
 		String email = "larissafabiao@gmail.com";
 		//texto de exemplo com exatamente 400 caracteres
 		String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et placerat purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque vulputate libero tortor. Fusce arcu felis, rhoncus eget est in, suscipit venenatis orci. Vestibulum eget feugiat justo. Morbi sit amet felis finibus, imperdiet metus eget, pellentesque risus. Nulla at porttitor turpis.";
-
-		Database database = new Database();
+		this.author = new Author(name, email, description);
 		
-		Author newAuthorOne = new Author(name, email, description);
+		this.category = new Category("terror");
 		
-		Map<String, Author> authors;
-		authors = database.addNewAuthor(newAuthorOne);
+		String title = "Neuromancer";
+		String synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue sollicitudin fermentum. Morbi maximus urna sed aliquet condimentum. Suspendisse porta aliquam lacus. Integer faucibus hendrerit sapien. Duis quis efficitur sapien, in sollicitudin sapien. Nunc luctus sit amet odio eget euismod. Maecenas quis interdum nisl. Nullam lectus augue, vestibulum quis nisi nec, sollicitudin mollis magna. Donec faucibus rhoncus lorem, tincidunt pulvinar augue vulputate nec. Pellentesque ornare morbi.";
+		String summary = "It's very easy to make some words **bold** and other words *italic* with Markdown.";
+		BigDecimal price = new BigDecimal("30.0");
+		int pages = 300;
+		String isbn = "Livro2";
+		LocalDate publicationDate = LocalDate.of(2020,8,16);
+		this.book = new Book(title, synopsis, summary, price, pages, isbn, category, publicationDate, author);
+	}
+	
+	@Test
+	public void testWithOneAutor() {
+		Map<String, Author> authors = database.addNewAuthor(author);
 		
-		Assert.assertTrue(authors.containsValue(newAuthorOne));	
+		Assert.assertTrue(authors.containsValue(author));	
 		Assert.assertEquals(authors.size(), 1);
 	}
 	
 	@Test
-	public void testWithTwoAuthors() {
-		String name = "Larissa Fabião da Fonseca";
-		String email = "larissafabiao@gmail.com";
-		//texto de exemplo com exatamente 400 caracteres
-		String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et placerat purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque vulputate libero tortor. Fusce arcu felis, rhoncus eget est in, suscipit venenatis orci. Vestibulum eget feugiat justo. Morbi sit amet felis finibus, imperdiet metus eget, pellentesque risus. Nulla at porttitor turpis.";
-		
+	public void testWithTwoAuthorsWithDiferentEmails() {
 		String nameTwo = "Larissa Fonseca";
 		String emailTwo = "larissao@gmail.com";
 		//texto de exemplo com menos de 400 caracteres
 		String descriptionTwo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque vulputate libero tortor. Fusce arcu felis, rhoncus eget est in, suscipit venenatis orci.  Nulla at porttitor turpis.";
 		
-		Database database = new Database();
-		
-		Author newAuthorOne = new Author(name, email, description);
 		Author newAuthorTwo = new Author(nameTwo, emailTwo, descriptionTwo);
 		
-		Map<String, Author> authors = database.addNewAuthor(newAuthorOne);
+		Map<String, Author> authors = database.addNewAuthor(author);
 		authors = database.addNewAuthor(newAuthorTwo);
-		Assert.assertTrue(authors.containsValue(newAuthorOne));	
+		Assert.assertTrue(authors.containsValue(author));	
 		Assert.assertTrue(authors.containsValue(newAuthorTwo));
-		Assert.assertEquals(authors.get(email.toUpperCase()), newAuthorOne);
+		Assert.assertEquals(authors.get(author.getEmail().toUpperCase()), author);
 		Assert.assertEquals(authors.get(emailTwo.toUpperCase()), newAuthorTwo);
 		Assert.assertEquals(authors.size(), 2);
 	}
 	
 	@Test
 	public void testWithAnEmailPreviouslyCadastredReturningAnException() {
-		String name = "Larissa Fabião da Fonseca";
-		String email = "larissafabiao@gmail.com";
-		//texto de exemplo com exatamente 400 caracteres
-		String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et placerat purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque vulputate libero tortor. Fusce arcu felis, rhoncus eget est in, suscipit venenatis orci. Vestibulum eget feugiat justo. Morbi sit amet felis finibus, imperdiet metus eget, pellentesque risus. Nulla at porttitor turpis.";
-		
-		String nameTwo = "Larissa Fonseca";
-		String emailTwo = "larissafabiao@gmail.com";
-		//texto de exemplo com menos de 400 caracteres
-		String descriptionTwo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque vulputate libero tortor. Fusce arcu felis, rhoncus eget est in, suscipit venenatis orci.  Nulla at porttitor turpis.";
-		
-		Database database = new Database();
-		
-		Author newAuthorOne = new Author(name, email, description);
-		Author newAuthorTwo = new Author(nameTwo, emailTwo, descriptionTwo);
-		
-		database.addNewAuthor(newAuthorOne);
-		Assertions.assertThrows(IllegalStateException.class, () -> database.addNewAuthor(newAuthorTwo));
+		database.addNewAuthor(author);
+		Assertions.assertThrows(IllegalStateException.class, () -> database.addNewAuthor(author));
 	}
 	
 	@Test
 	public void searchingForAnAuthorPreviouslyCadastredAndReturningThisAuthor() {
-		String name = "Larissa Fabião da Fonseca";
-		String email = "larissafabiao@gmail.com";
-		//texto de exemplo com exatamente 400 caracteres
-		String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et placerat purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque vulputate libero tortor. Fusce arcu felis, rhoncus eget est in, suscipit venenatis orci. Vestibulum eget feugiat justo. Morbi sit amet felis finibus, imperdiet metus eget, pellentesque risus. Nulla at porttitor turpis.";
-		
 		String nameTwo = "Larissa Fonseca";
 		String emailTwo = "larissao@gmail.com";
 		//texto de exemplo com menos de 400 caracteres
 		String descriptionTwo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque vulputate libero tortor. Fusce arcu felis, rhoncus eget est in, suscipit venenatis orci.  Nulla at porttitor turpis.";
 		
-		Database database = new Database();
-		
-		Author newAuthorOne = new Author(name, email, description);
 		Author newAuthorTwo = new Author(nameTwo, emailTwo, descriptionTwo);
-		database.addNewAuthor(newAuthorOne);
+		
+		database.addNewAuthor(author);
 		database.addNewAuthor(newAuthorTwo);
 		
+		Assert.assertEquals(author, database.searchAuthor(author.getEmail()));
 		Assert.assertEquals(newAuthorTwo, database.searchAuthor(emailTwo));
 	}
 	
 	@Test
 	public void searchingForAnAuthorNotCadastredAndReturningAIllegalStateException() {
-		String name = "Larissa Fabião da Fonseca";
-		String email = "larissafabiao@gmail.com";
-		//texto de exemplo com exatamente 400 caracteres
-		String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et placerat purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque vulputate libero tortor. Fusce arcu felis, rhoncus eget est in, suscipit venenatis orci. Vestibulum eget feugiat justo. Morbi sit amet felis finibus, imperdiet metus eget, pellentesque risus. Nulla at porttitor turpis.";
-				
-		Database database = new Database();
-		
-		Author newAuthorOne = new Author(name, email, description);
-	
-		database.addNewAuthor(newAuthorOne);
-		
+		database.addNewAuthor(author);
 		Assert.assertThrows(IllegalStateException.class , () -> database.searchAuthor("larissao@gmail.com"));
 	}
 	
 	@Test
 	public void includingAnBookAndReturningASuccessOperation() {
-		String title = "O pequeno Principe";
-		String synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue sollicitudin fermentum. Morbi maximus urna sed aliquet condimentum. Suspendisse porta aliquam lacus. Integer faucibus hendrerit sapien. Duis quis efficitur sapien, in sollicitudin sapien. Nunc luctus sit amet odio eget euismod. Maecenas quis interdum nisl. Nullam lectus augue, vestibulum quis nisi nec, sollicitudin mollis magna. Donec faucibus rhoncus lorem, tincidunt pulvinar augue vulputate nec. Pellentesque ornare morbi.";
-		String summary = "It's very easy to make some words **bold** and other words *italic* with Markdown.";
-		BigDecimal price = new BigDecimal("25.0");
-		int pages = 120;
-		String isbn = "Livro1";
-		LocalDate publicationDate = LocalDate.of(2020,8,16);
-		Category category = new Category("clássico moderno");
-		
-		String name = "Fernando Boaglio";
-		String email = "larissafabiao@gmail.com";
-		String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et placerat purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque vulputate libero tortor. Fusce arcu felis, rhoncus eget est in, suscipit venenatis orci. Vestibulum eget feugiat justo. Morbi sit amet felis finibus, imperdiet metus eget, pellentesque risus. Nulla at porttitor turpis.";
-		
-		Author fernandoBoaglio = new Author(name, email, description);
-		
-		Book oPequenoPrincipe = new Book(title, synopsis, summary, price, pages, isbn, category, publicationDate, fernandoBoaglio);
-		Database database = new Database();
-		database.addBookInDatabase(oPequenoPrincipe);
-		
+		database.addBookInDatabase(book);
 	}
 	
 	@Test
@@ -149,27 +110,10 @@ public class DatabaseTest {
 		String isbn = "Livro1";
 		LocalDate publicationDate = LocalDate.of(2020,8,16);
 		Category category = new Category("clássico moderno");
-		
-		String title2 = "Neuromancer";
-		String synopsis2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue sollicitudin fermentum. Morbi maximus urna sed aliquet condimentum. Suspendisse porta aliquam lacus. Integer faucibus hendrerit sapien. Duis quis efficitur sapien, in sollicitudin sapien. Nunc luctus sit amet odio eget euismod. Maecenas quis interdum nisl. Nullam lectus augue, vestibulum quis nisi nec, sollicitudin mollis magna. Donec faucibus rhoncus lorem, tincidunt pulvinar augue vulputate nec. Pellentesque ornare morbi.";
-		String summary2 = "It's very easy to make some words **bold** and other words *italic* with Markdown.";
-		BigDecimal price2 = new BigDecimal("30.0");
-		int pages2 = 300;
-		String isbn2 = "Livro2";
-		LocalDate publicationDate2 = LocalDate.of(2020,8,16);
-		Category category2 = new Category("futurista");
-		
-		String name = "Fernando Boaglio";
-		String email = "larissafabiao@gmail.com";
-		String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et placerat purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque vulputate libero tortor. Fusce arcu felis, rhoncus eget est in, suscipit venenatis orci. Vestibulum eget feugiat justo. Morbi sit amet felis finibus, imperdiet metus eget, pellentesque risus. Nulla at porttitor turpis.";
-		
-		Author fernandoBoaglio = new Author(name, email, description);
-		
-		Book oPequenoPrincipe = new Book(title, synopsis, summary, price, pages, isbn, category, publicationDate, fernandoBoaglio);
-		Book neuromancer = new Book(title2, synopsis2, summary2, price2, pages2, isbn2, category2, publicationDate2, fernandoBoaglio);
-		Database database = new Database();
+				
+		Book oPequenoPrincipe = new Book(title, synopsis, summary, price, pages, isbn, category, publicationDate, author);
 		database.addBookInDatabase(oPequenoPrincipe);
-		Map<String, Book> books = database.addBookInDatabase(neuromancer);
+		Map<String, Book> books = database.addBookInDatabase(book);
 		Assert.assertEquals(books.size(), 2);
 	}
 	
@@ -184,69 +128,40 @@ public class DatabaseTest {
 		LocalDate publicationDate = LocalDate.of(2020,8,16);
 		Category category = new Category("clássico moderno");
 		
-		String title2 = "Neuromancer";
-		String synopsis2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue sollicitudin fermentum. Morbi maximus urna sed aliquet condimentum. Suspendisse porta aliquam lacus. Integer faucibus hendrerit sapien. Duis quis efficitur sapien, in sollicitudin sapien. Nunc luctus sit amet odio eget euismod. Maecenas quis interdum nisl. Nullam lectus augue, vestibulum quis nisi nec, sollicitudin mollis magna. Donec faucibus rhoncus lorem, tincidunt pulvinar augue vulputate nec. Pellentesque ornare morbi.";
-		String summary2 = "It's very easy to make some words **bold** and other words *italic* with Markdown.";
-		BigDecimal price2 = new BigDecimal("30.0");
-		int pages2 = 300;
-		String isbn2 = "Livro1";
-		LocalDate publicationDate2 = LocalDate.of(2020,8,16);
-		Category category2 = new Category("futurista");
-		
-		String name = "Fernando Boaglio";
-		String email = "larissafabiao@gmail.com";
-		String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et placerat purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque vulputate libero tortor. Fusce arcu felis, rhoncus eget est in, suscipit venenatis orci. Vestibulum eget feugiat justo. Morbi sit amet felis finibus, imperdiet metus eget, pellentesque risus. Nulla at porttitor turpis.";
-		
-		Author fernandoBoaglio = new Author(name, email, description);
-		
-		Book oPequenoPrincipe = new Book(title, synopsis, summary, price, pages, isbn, category, publicationDate, fernandoBoaglio);
-		Book neuromancer = new Book(title2, synopsis2, summary2, price2, pages2, isbn2, category2, publicationDate2, fernandoBoaglio);
-		
-		Database database = new Database();
+		Book oPequenoPrincipe = new Book(title, synopsis, summary, price, pages, isbn, category, publicationDate, author);
 		database.addBookInDatabase(oPequenoPrincipe);
-		Assert.assertThrows(IllegalStateException.class,() -> database.addBookInDatabase(neuromancer));
+		Assert.assertThrows(IllegalStateException.class,() -> database.addBookInDatabase(book));
 	}
 	
 	@Test
 	public void testeWithOneCategoryWithValidData() {
-		Database database = new Database();
-		Category categoryCreted = new Category("terror");
-		
-		Map<String, Category> categories = database.addNewCategory(categoryCreted);
+		Map<String, Category> categories = database.addNewCategory(category);
 		Assert.assertEquals(categories.size(), 1);
 	}
 	
 	@Test
 	public void testeWithTheInsertionOfTwoCatgoriesWithTheSameName() {
-		Database database = new Database();
-		Category categoryOne = new Category("terror");
-		Category categoryTwo = new Category("Terror");
+		Category categoryTwo = new Category("Tecnico");
 		
-		database.addNewCategory(categoryOne);
+		database.addNewCategory(category);
 		
 		Assert.assertThrows(IllegalStateException.class, () -> database.addNewCategory(categoryTwo));
 	}
 	
 	@Test
 	public void insertTwoDifferentCategoriesAndSearchForThenReturningTheCorrectObject() {
-		Database database = new Database();
-		Category categoryOne = new Category("terror");
 		Category categoryTwo = new Category("ficção científica");
 		
-		database.addNewCategory(categoryOne);
+		database.addNewCategory(category);
 		Map<String, Category> categories = database.addNewCategory(categoryTwo);
 		
-		Assert.assertEquals(categoryOne, database.searchCategory("Terror"));
+		Assert.assertEquals(category, database.searchCategory("Terror"));
 		Assert.assertEquals(categoryTwo, database.searchCategory("ficção científica"));
 	}
 	
 	@Test
 	public void testeToSearchForAnInexistentCategoryReturningAnException() {
-		Database database = new Database();
-		Category categoryOne = new Category("terror");
-		
-		Map<String, Category> categories = database.addNewCategory(categoryOne);
-		
+		Map<String, Category> categories = database.addNewCategory(category);
 		Assert.assertThrows(IllegalStateException.class, () -> database.searchCategory("suspense"));
 	}
 	
